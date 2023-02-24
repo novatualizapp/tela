7bdc3da6fcaae89fb3067c383c1cf75b0052d27d
nova atualiza <novatualizapp@gmail.com>
2023-02-23 22:48:21 -0200

    VAI1

--- src/home.js
+++ src/home.js
@@ -5,7 +5,6 @@ import alertify from 'alertifyjs';
 import InputMask from 'react-input-mask'
 import logo from './Assets/Images/logo1.png'
 import Loader from './Components/Loader';
-import './home.css';
 
 const useStyles = makeStyles((theme) => ({
     root: {
